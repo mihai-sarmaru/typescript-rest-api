620209f325b56c9aeae34be4d64ecf0b8e6b2c99
Mihai Sarmaru <mihai.sarmaru@gmail.com>
2020-11-11 15:52:17 +0200

    Add openapi validation for express server

--- src/utils/server.ts
+++ src/utils/server.ts
@@ -1,12 +1,37 @@
 import express from 'express';
 import {Express} from 'express-serve-static-core';
+import * as OpenApiValidator from 'express-openapi-validator';
+import {connector, summarise} from 'swagger-routes-express';
+import YAML from 'yamljs';
+
+import * as api from '../api/controllers';
 
 export async function createServer(): Promise<Express> {
+    // load openapi yaml file
+    const yamlSpecFile = './config/openapi.yml';
+    const apiDefinition = YAML.load(yamlSpecFile);
+    const apiSummary = summarise(apiDefinition);
+    console.info(apiSummary);
+
+    // init express server
     const server = express();
 
-    server.get('/', (req, res) => {
-        res.send('Hello world!');
+    // API validator
+    const validatorOptions = {
+        coerceTypes: true,
+        apiSpec: yamlSpecFile,
+        validateRequests: true,
+        validateResponses: true
+    };
+    server.use(OpenApiValidator.middleware(validatorOptions));
+
+    // connect server with api definition
+    const connect = connector(api, apiDefinition, {
+        onCreateRoute: (method: string, descriptor: any[]) => {
+            console.log(`${method}: ${descriptor[0]} : ${(descriptor[1] as any).name}`);
+        }
     });
+    connect(server);
 
     return server;
 }
