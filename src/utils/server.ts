ef3b3ea2d1bff3bb3eeeebeaf949bcd503a3a29a
Mihai Sarmaru <mihai.sarmaru@gmail.com>
2020-11-11 17:01:32 +0200

    Ass security option to openapi express calling auth function

--- src/utils/server.ts
+++ src/utils/server.ts
@@ -38,7 +38,11 @@ export async function createServer(): Promise<Express> {
     // connect server with api definition
     const connect = connector(api, apiDefinition, {
         onCreateRoute: (method: string, descriptor: any[]) => {
-            console.log(`${method}: ${descriptor[0]} : ${(descriptor[1] as any).name}`);
+            descriptor.shift();
+            console.log(`${method}: ${descriptor.map((d: any) => d.name).join(', ')}`);
+        },
+        security: {
+            bearerAuth: api.auth
         }
     });
     connect(server);
